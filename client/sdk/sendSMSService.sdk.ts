7ed24bbffe6b7f1553dfcce70610cd9a288db12a
stefandaius <stefan.iordache2001@gmail.com>
2023-02-17 13:00:47 +0200

    add

--- client/sdk/sendSMSService.sdk.ts
+++ client/sdk/sendSMSService.sdk.ts
@@ -10,12 +10,8 @@ import { Remote } from "./remote"
 export class SendSMSService {
     static remote = new Remote("http://127.0.0.1:8083/SendSMSService")
 
-    static async sendTestMessage(): Promise<any> {
-        return await SendSMSService.remote.call("SendSMSService.sendTestMessage")  
-  }
-
-  static async test(): Promise<any> {
-        return await SendSMSService.remote.call("SendSMSService.test")  
+    static async sendTestMessage(message: string, to: string): Promise<string> {
+        return await SendSMSService.remote.call("SendSMSService.sendTestMessage", message, to)  
   }
 
   

e6b35e1d962ae4611e390dd6306f2a50b97c24a1
stefandaius <stefan.iordache2001@gmail.com>
2023-02-17 16:37:46 +0200

    promises

--- server/endpoints/cron.ts
+++ server/endpoints/cron.ts
@@ -13,21 +13,24 @@ export class SendMessageCron {
         const openAIAssistant = new OpenAIAssistant();
         const sendMessageService = new SendMessageService();
         const users = await UserModel.find();
+        const promises: Promise<any>[] = [];
         for (const user of users) {
             if (user.settings) {
                 const hour = new Date().getHours();
                 if (hour == user.settings.hour) {
                     console.log(`DEBUG: Send message to user ${user.name}`);
                     const request = "Write a " + user.settings?.poemSettings?.poemAdjective + " poem about  " + user.settings?.poemSettings?.words.join(",") + " for my partener" + ".\n";
-                    const response = await openAIAssistant.askChatGPT(request);
-                    if (response) {
-                        console.log(user.settings.phone);
-                        sendMessageService.sendTestMessage(response, user.settings?.phone);
-                        const userService = new UserService();
-                        await userService.addPoem(user._id.toString(), response);
-                    }
+                    const response = openAIAssistant.askChatGPT(request).then(async (response) => {
+                        if (response) {
+                            sendMessageService.sendTestMessage(response, user.settings?.phone);
+                            const userService = new UserService();
+                            await userService.addPoem(user._id.toString(), response);
+                        }
+                    });
+                    promises.push(response);
                 }
             }
         }
+        await Promise.all(promises);
     }
 }

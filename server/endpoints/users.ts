566547fa662117b2223f2fd99163e3210c4c486f
Bogdan Vlad <bogdan@genez.io>
2023-02-17 13:24:29 +0200

    Create structure.

---
+++ server/endpoints/users.ts
@@ -0,0 +1,16 @@
+type UserSettings = {
+    phone: string,
+    poemSettings: {
+        poemAdjective: string,
+        words: string[],
+    },
+    hour: number,
+}
+
+export class UserRepository {
+    // login
+    // signup
+    // check token
+    // set preferences
+    // list current preferences 
+}

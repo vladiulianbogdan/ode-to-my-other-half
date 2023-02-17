0f24e427d5dcf64409c655bbfc4d7b09a740769f
stefandaius <stefan.iordache2001@gmail.com>
2023-02-17 12:58:21 +0200

    send message function

--- server/sendSmsService.ts
+++ server/sendSmsService.ts
@@ -1,11 +1,19 @@
 export class SendSMSService {
-	sendTestMessage() {
-        console.log("sendTestMessage")
+	async sendTestMessage(message: string, to: string): Promise<string> {
+        require('dotenv').config();
+        const accountSid: string | undefined = process.env.TWILIO_ACCOUNT_SID;
+        const authToken: string | undefined = process.env.TWILIO_AUTH_TOKEN;
+        console.log(accountSid, authToken);
 
-        return "hello222333!"
-    }
+        const client: any = require('twilio')(accountSid, authToken);
 
-    test() {
-        
+        const msg = await client.messages.create({
+            body: message,
+            from: "whatsapp:+14155238886",
+            to: "whatsapp:" + to
+        });
+
+        console.log(msg.sid);
+        return "Message sent!";
     }
 }
